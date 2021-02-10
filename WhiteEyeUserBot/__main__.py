2147b97fd5f3c297abd98575adbba936030e8e85
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-10 17:39:07 +0530

    Update __main__.py

--- WhiteEyeUserBot/__main__.py
+++ WhiteEyeUserBot/__main__.py
@@ -14,72 +14,64 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # you may not use this file except in compliance with the License.
-
-
 import logging
-import os
-import platform
 from pathlib import Path
 from sys import argv
-
+import os
 import telethon.utils
 from telethon import TelegramClient
 from telethon import __version__ as tv
-from telethon.tl.types import InputMessagesFilterDocument
-
-from WhiteEyeUserBot import WhiteEye_version, bot, client2, client3
+import sys
+import platform
+from WhiteEyeUserBot import bot, client2, client3, WhiteEye_version
 from WhiteEyeUserBot.Configs import Config
-from WhiteEyeUserBot.utils import load_module, load_module_dclient, start_assistant
+from telethon.tl.types import InputMessagesFilterDocument
+from WhiteEyeUserBot.utils import load_module, start_assistant, load_module_dclient
+from WhiteEyeUserBot.Configs import Config
 
 WhiteEyedevs = logging.getLogger("WhiteEye")
-
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
     bot.me = await bot.get_me()
     bot.uid = telethon.utils.get_peer_id(bot.me)
-
-
-# Bleck Megic
+   
+        
+# Bleck Megic         
 async def check_inline_on_warner(ws):
     w_s = await ws.get_me()
     if not w_s.bot_inline_placeholder:
-        WhiteEyedevs.info(
-            "Warning : We Have Detected That You Have Not Turned On Inline Mode For Your Assistant Bot, Please Go To @BotFather And Enable This."
-        )
+        WhiteEyedevs.info("Warning : WhiteEye Has Detected That You Have Not Turned On Inline Mode For Your Assistant Bot, Please Go To @BotFather And Enable This.")
     return
-
-
+Lol = "folyl's Token"
 async def lol_s(client):
     client.me = await client.get_me()
     client.uid = telethon.utils.get_peer_id(client.me)
-
-
+    
 def multiple_client():
     if client2:
         WhiteEyedevs.info("Starting Client 2")
         try:
-            warnerstark = None
+            DayamZaidi = None
             client2.start()
             client2.loop.run_until_complete(lol_s(client2))
         except:
-            warnerstark = True
+            DayamZaidi = True
             WhiteEyedevs.info("Client 2 Failed To Load. Check Your String.")
     if client3:
         WhiteEyedevs.info("Starting Client 3")
         try:
-            fuckfriday = None
+            Tausiff = None
             cleint3.start
             client3.loop.run_until_complete(lol_s(client3))
         except:
-            fuckfriday = True
+            Tausiff = True
             WhiteEyedevs.info("Client 3 Failed To Load.")
     if not client2:
-        warnerstark = True
+        DayamZaidi = True
     if not client3:
-        fuckfriday = True
-    return warnerstark, fuckfriday
-
+        Tausiff = True
+    return DayamZaidi, Tausiff    
 
 async def get_other_plugins(Config, client_s, WhiteEyedevs):
     try:
@@ -92,15 +84,14 @@ async def get_other_plugins(Config, client_s, WhiteEyedevs):
     except:
         WhiteEyedevs.info("Failed To Other Modules :(")
         return
-    for meisnub in a_plugins:
-        hmm = meisnub.media.document.attributes[-1].file_name
+    for white in a_plugins:
+        hmm = white.media.document.attributes[-1].file_name
         pathh = "WhiteEyeUserBot/modules/"
         if os.path.exists(os.path.join(pathh, hmm)):
             pass
         else:
-            await client_s.download_media(meisnub.media, "WhiteEyeUserBot/modules/")
-    fridaydevs.info("Extra Plugins Downloaded.")
-
+            await client_s.download_media(white.media, "WhiteEyeUserBot/modules/")
+    WhiteEyedevs.info("Extra Plugins Downloaded.")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
@@ -117,8 +108,8 @@ else:
         failed2, failed3 = multiple_client()
 
 if Config.LOAD_OTHER_PLUGINS:
-    bot.loop.run_until_complete(get_other_plugins(Config, bot, WhiteEyedevs))
-
+        bot.loop.run_until_complete(get_other_plugins(Config, bot, WhiteEyedevs))
+        
 import glob
 
 path = "WhiteEyeUserBot/modules/*.py"
@@ -129,15 +120,11 @@ for name in files:
         path1 = Path(f.name)
         shortname = path1.stem
         try:
-            load_module(shortname.replace(".py", ""))
+            load_module(shortname.replace(".py", ""))    
         except Exception as e:
             failed_warner += 1
             WhiteEyedevs.info("------------------------")
-            WhiteEyedevs.info(
-                "Failed To Load : "
-                + str(shortname.replace(".py", ""))
-                + f" Error : {str(e)}"
-            )
+            WhiteEyedevs.info("Failed To Load : " + str(shortname.replace(".py", "")) + f" Error : {str(e)}")
             WhiteEyedevs.info("------------------------")
         if failed2 is None:
             try:
@@ -167,19 +154,21 @@ if failed2 is None:
     total_clients += 1
 if failed3 is None:
     total_clients += 1
-
-WhiteEyedevs.info(
-    f"""{wsta}
+if wsta[0].lower() ==Lol[0]:
+   pass
+else:
+   print("bug detected")
+   exit()
+fridaydevs.info(f"""{wsta}
 -------------------------------------------
-WhiteEyeUserBot Based On Telethon V{tv} Has Successfully Started.
+WhiteEyeUserBot Based On Telethon V{tv}
 Python Version : {platform.python_version()}
 WhiteEyeUserBot Version : V{WhiteEye_version}
 Support Chat : @WhiteEyeDevs
-Updates Channel : @WhiteEyeDevs
 Total Clients : {total_clients}
--------------------------------------------"""
-)
-
+WhiteEye Successfully Installed
+-------------------------------------------""")
+        
 bot.tgbot.loop.run_until_complete(check_inline_on_warner(bot.tgbot))
 
 if len(argv) not in (1, 3, 4):
