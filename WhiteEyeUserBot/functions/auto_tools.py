5815c6757289031c2103ab3ba8d16aa2f599f179
mrdayamzaidi <dayamali975@gmail.com>
2021-02-13 09:22:10 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/functions/auto_tools.py
+++ WhiteEyeUserBot/functions/auto_tools.py
@@ -1,24 +1,26 @@
-import asyncio
-import time
-import random
-from telethon.tl import functions
-from WhiteEyeUserBot.Configs import Config
-from WhiteEyeUserBot import bot
-import pytz
-import asyncio
+import logging
 import os
-import shutil
+import random
 from datetime import datetime
+
+import pytz
 from PIL import Image, ImageDraw, ImageFont
 from pySmartDL import SmartDL
 from telethon.tl import functions
-import asyncio
-import os
-import logging
-from datetime import datetime
+
+from WhiteEyeUserBot import bot
+from WhiteEyeUserBot.Configs import Config
+
 m = logging.getLogger("AUTOTOOLS")
 
-bio_temp = ["Making History.", "I'm on energy-saving mode.", "Success is in my blood.", "Life F$@ks me, now it's my turn.", "Error 404: Bio unavailable."]
+bio_temp = [
+    "Making History.",
+    "I'm on energy-saving mode.",
+    "Success is in my blood.",
+    "Life F$@ks me, now it's my turn.",
+    "Error 404: Bio unavailable.",
+]
+
 
 async def auto_name(name=None):
     TZ = pytz.timezone(Config.TZ)
@@ -26,31 +28,26 @@ async def auto_name(name=None):
     nameof = name if name else Config.ALIVE_NAME
     oof = datetime_tz.strftime(f"🕒 %d/%m/%Y ⚡{nameof}⚡ 📅 %H:%M")
     try:
-        await bot(
-                functions.account.UpdateProfileRequest(
-                    first_name=oof
-                )
-            )
+        await bot(functions.account.UpdateProfileRequest(first_name=oof))
     except Exception as e:
         m.warning(f"Failed AutoName Due To : {e}")
-    return    
-    
+    return
+
+
 async def auto_bio(bio=None):
     TZ = pytz.timezone(Config.TZ)
     datetime_tz = datetime.now(TZ)
     bioof = bio if bio else random.choice(bio_temp)
     oof = datetime_tz.strftime(f'🕒%d/%m/%Y "{bioof}" 📅%H:%M')
     try:
-        await bot(
-                functions.account.UpdateProfileRequest(
-                    about=oof
-                )
-            )
+        await bot(functions.account.UpdateProfileRequest(about=oof))
     except Exception as e:
         m.warning(f"Failed AutoBio Due To : {e}")
-    return    
+    return
+
 
 counter = -30
+
 
 async def auto_pic():
     global counter
@@ -59,34 +56,36 @@ async def auto_pic():
     downloaded_file_name = "WhiteEyeUserBot/original_pic.png"
     if not os.path.exists(downloaded_file_name):
         downloader = SmartDL(
-        Config.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False
-            )
+            Config.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False
+        )
         downloader.start(blocking=False)
-        photo = "fridaybot/photo_pfp.png"
         while not downloader.isFinished():
             pass
     else:
         pass
     img = Image.open(downloaded_file_name)
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype('Fonts/Streamster.ttf', 220)
+    font = ImageFont.truetype("Fonts/Streamster.ttf", 220)
     file_test = img.rotate(counter, expand=False)
     image_widthz, image_heightz = img.size
     TZ = pytz.timezone(Config.TZ)
     datetime_tz = datetime.now(TZ)
-    text = datetime_tz.strftime('%H:%M')
-    w,h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    text = datetime_tz.strftime("%H:%M")
+    w, h = draw.textsize(text, font=font)
+    h += int(h * 0.21)
+    draw.text(
+        ((image_widthz - w) / 2, (image_heightz - h) / 2),
+        text,
+        font=font,
+        fill=(255, 255, 255),
+    )
     file_name = "autopic_friday.png"
     img.save(file_name, "PNG")
     file = await bot.upload_file(file_name)
     try:
-            await bot(
-                functions.photos.UploadProfilePhotoRequest(file)
-            )
-            os.remove(file_name)
-            counter -= 30
+        await bot(functions.photos.UploadProfilePhotoRequest(file))
+        os.remove(file_name)
+        counter -= 30
     except:
-            return
+        return
     return
