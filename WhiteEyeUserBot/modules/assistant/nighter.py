7de3178f21ecd08e98184fc34e4bc55a2bc753c2
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-13 13:00:12 +0530

    Update nighter.py

--- WhiteEyeUserBot/modules/assistant/nighter.py
+++ WhiteEyeUserBot/modules/assistant/nighter.py
@@ -82,7 +82,7 @@ async def job_close():
 
 
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(job_close, trigger="cron", hour=13, minute=0)
+scheduler.add_job(job_close, trigger="cron", hour=13, minute=2)
 scheduler.start()
 
 
