d9fbbcbce7d5335f8291a3b4b6c3a4c6725917d9
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-14 19:35:57 +0530

    Update image_vidtools.py

--- WhiteEyeUserBot/modules/image_vidtools.py
+++ WhiteEyeUserBot/modules/image_vidtools.py
@@ -804,7 +804,7 @@ async def yufytf(event):
         return
     await event.edit("`Processing..`")
     text = event.pattern_match.group(2)
-    img = Image.open("./resources/zipper.png")
+    img = Image.open("./resources/white.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("Fonts/PermanentMarker-Regular.ttf", 200)
     image_widthz, image_heightz = img.size
