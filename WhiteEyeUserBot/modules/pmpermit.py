6942be70b21e13625cfa19d5aa95c5f98fa4d963
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-14 19:04:06 +0530

    Update pmpermit.py

--- WhiteEyeUserBot/modules/pmpermit.py
+++ WhiteEyeUserBot/modules/pmpermit.py
@@ -12,7 +12,7 @@ from WhiteEyeUserBot.utils import WhiteEye_on_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-    WARN_PIC = "https://telegra.ph/file/63d2f8bcdae4da2ec5e7e.jpg"
+    WARN_PIC = "https://telegra.ph/file/e41d27e913a961d105d4d.jpg"
 else:
     WARN_PIC = PMPERMIT_PIC
 
