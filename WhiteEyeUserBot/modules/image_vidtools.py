82d793e481358ea91e6c10db5798f12d7fe88e38
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-14 19:48:09 +0530

    Update image_vidtools.py

--- WhiteEyeUserBot/modules/image_vidtools.py
+++ WhiteEyeUserBot/modules/image_vidtools.py
@@ -806,7 +806,7 @@ async def yufytf(event):
     text = event.pattern_match.group(2)
     img = Image.open("./resources/linker.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("Fonts/GreatVibes-Regular.ttf", 50)
+    font = ImageFont.truetype("Fonts/star/Chopsic.otf", 100)
     image_widthz, image_heightz = img.size
     w, h = draw.textsize(text, font=font)
     h += int(h * 0.21)
