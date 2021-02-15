83e59426524e04b6b1b9e550061b9dfb924818c8
mrdayamzaidi <dayamali975@gmail.com>
2021-02-15 12:45:39 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/modules/sql_helper/auto_post_sql.py
+++ WhiteEyeUserBot/modules/sql_helper/auto_post_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from sqlalchemy import Column, String, UnicodeText, distinct, func
+from sqlalchemy import Column, String
 
 from . import BASE, SESSION
 
@@ -15,7 +15,10 @@ class Post(BASE):
         self.target_chat_id = str(target_chat_id)
 
     def __repr__(self):
-        return "<Auto post filter '%s' for %s>" % (self.target_chat_id, self.to_post_chat_id)
+        return "<Auto post filter '%s' for %s>" % (
+            self.target_chat_id,
+            self.to_post_chat_id,
+        )
 
     def __eq__(self, other):
         return bool(
@@ -24,8 +27,6 @@ class Post(BASE):
             and self.to_post_chat_id == other.to_post_chat_id
         )
 
-        
-
 
 Post.__table__.create(checkfirst=True)
 
@@ -33,11 +34,12 @@ POST_FILTER_INSERTION_LOCK = threading.RLock()
 
 CHAT_POSTS = {}
 
+
 def add_new_post_data_in_db(target_chat_id: str, to_post_chat_id: str):
     with POST_FILTER_INSERTION_LOCK:
         blacklist_filt = Post(str(target_chat_id), str(to_post_chat_id))
 
-        SESSION.merge(blacklist_filt)  
+        SESSION.merge(blacklist_filt)
         SESSION.commit()
         CHAT_POSTS.setdefault(str(target_chat_id), set()).add(str(to_post_chat_id))
 
@@ -48,15 +50,21 @@ def get_all_post_data(target_chat_id: str):
 
 def is_post_data_in_db(target_chat_id, to_post_chat_id):
     with POST_FILTER_INSERTION_LOCK:
-        broadcast_group = SESSION.query(Post).get((str(target_chat_id), str(to_post_chat_id)))
+        broadcast_group = SESSION.query(Post).get(
+            (str(target_chat_id), str(to_post_chat_id))
+        )
         return bool(broadcast_group)
 
 
 def remove_post_data(target_chat_id, to_post_chat_id):
     with POST_FILTER_INSERTION_LOCK:
-        blacklist_filt = SESSION.query(Post).get((str(target_chat_id), str(to_post_chat_id)))
+        blacklist_filt = SESSION.query(Post).get(
+            (str(target_chat_id), str(to_post_chat_id))
+        )
         if blacklist_filt:
-            if str(to_post_chat_id) in CHAT_POSTS.get(str(target_chat_id), set()):  # sanity check
+            if str(to_post_chat_id) in CHAT_POSTS.get(
+                str(target_chat_id), set()
+            ):  # sanity check
                 CHAT_POSTS.get(str(target_chat_id), set()).remove(str(to_post_chat_id))
 
             SESSION.delete(blacklist_filt)
@@ -66,11 +74,12 @@ def remove_post_data(target_chat_id, to_post_chat_id):
         SESSION.close()
         return False
 
+
 def __load_chat_channels():
     global CHAT_POSTS
     try:
         chats = SESSION.query(Post.target_chat_id).distinct().all()
-        for (target_chat_id,) in chats: 
+        for (target_chat_id,) in chats:
             CHAT_POSTS[target_chat_id] = []
 
         all_filters = SESSION.query(Post).all()
