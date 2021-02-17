6f799ca411b5505a3c75f0d68d26a575327cbb47
mrdayamzaidi <dayamali975@gmail.com>
2021-02-17 09:33:25 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/clients.py
+++ WhiteEyeUserBot/clients.py
@@ -1,6 +1,7 @@
 # All 3 Clients
 from telethon import TelegramClient
 from telethon.sessions import StringSession
+
 from WhiteEyeUserBot.Configs import Config
 
 if Config.STRING_SESSION:
@@ -10,10 +11,14 @@ else:
     session_name = "startup"
     bot = TelegramClient(session_name, Config.APP_ID, Config.API_HASH)
 if Config.STRING_SESSION_2:
-    client2 =  TelegramClient(StringSession(Config.STRING_SESSION_2), Config.APP_ID, Config.API_HASH)
+    client2 = TelegramClient(
+        StringSession(Config.STRING_SESSION_2), Config.APP_ID, Config.API_HASH
+    )
 else:
     client2 = None
 if Config.STRING_SESSION_3:
-    client3 =  TelegramClient(StringSession(Config.STRING_SESSION_3), Config.APP_ID, Config.API_HASH)
+    client3 = TelegramClient(
+        StringSession(Config.STRING_SESSION_3), Config.APP_ID, Config.API_HASH
+    )
 else:
     client3 = None
