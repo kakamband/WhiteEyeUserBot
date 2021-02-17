8ac85b4c78954062ad7a6d2d498ea4d594796420
mrdayamzaidi <dayamali975@gmail.com>
2021-02-17 09:29:39 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/Configs.py
+++ WhiteEyeUserBot/Configs.py
@@ -16,23 +16,16 @@
 # you may not use this file except in compliance with the License.
 
 import os
-import asyncio
-import logging
-import os
-import sys
-import time
 from distutils.util import strtobool as sb
-from telethon.tl.types import ChatBannedRights
-import pylast
-import wget
-from dotenv import load_dotenv
-from pylast import LastFMNetwork, md5
 
+import pylast
+from telethon.tl.types import ChatBannedRights
 
 ENV = bool(os.environ.get("ENV", False))
 if not ENV:
     from local_config import Development as Config
 elif ENV:
+
     class Config(object):
         LOGGER = True
         APP_ID = int(os.environ.get("APP_ID", 6))
@@ -43,7 +36,9 @@ elif ENV:
         OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
         REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
         CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/usr/bin/chromedriver")
-        GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/usr/bin/google-chrome")
+        GOOGLE_CHROME_BIN = os.environ.get(
+            "GOOGLE_CHROME_BIN", "/usr/bin/google-chrome"
+        )
         HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
         HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
         HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
@@ -72,26 +67,30 @@ elif ENV:
         LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
         LASTFM_PASS = pylast.md5(LASTFM_PASSWORD_PLAIN)
         if not LASTFM_USERNAME == "None":
-           lastfm = pylast.LastFMNetwork(
-               api_key=LASTFM_API,
-               api_secret=LASTFM_SECRET,
-               username=LASTFM_USERNAME,
-               password_hash=LASTFM_PASS,
-           )
+            lastfm = pylast.LastFMNetwork(
+                api_key=LASTFM_API,
+                api_secret=LASTFM_SECRET,
+                username=LASTFM_USERNAME,
+                password_hash=LASTFM_PASS,
+            )
         else:
-           lastfm = None
+            lastfm = None
         G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
         G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
         G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
         GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-        TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./downloads")
+        TEMP_DOWNLOAD_DIRECTORY = os.environ.get(
+            "TEMP_DOWNLOAD_DIRECTORY", "./downloads"
+        )
         BOTLOG = sb(os.environ.get("BOTLOG", "False"))
         TZ = os.environ.get("TZ", "Asia/Kolkata")
         API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
         CLEAN_GROUPS = os.environ.get("CLEAN_GROUPS", False)
         STRING_SESSION = os.environ.get("STRING_SESSION", None)
         DB_URI = os.environ.get("DATABASE_URL", None)
-        TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", './WhiteEyeUserBot/DOWNLOADS/')
+        TEMP_DOWNLOAD_DIRECTORY = os.environ.get(
+            "TEMP_DOWNLOAD_DIRECTORY", "./WhiteEyeUserBot/DOWNLOADS/"
+        )
         LOGGER = True
         GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
         GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
@@ -160,7 +159,9 @@ elif ENV:
         # Get a Free API Key from OCR.Space
         OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
         DEEP_API_KEY = os.environ.get("DEEP_API_KEY", None)
-        PING_SERVER_EVERY_MINUTE_VALUE = int(os.environ.get("PING_SERVER_EVERY_MINUTE_VALUE", 30))
+        PING_SERVER_EVERY_MINUTE_VALUE = int(
+            os.environ.get("PING_SERVER_EVERY_MINUTE_VALUE", 30)
+        )
         DEEZER_ARL_TOKEN = os.environ.get("DEEZER_ARL_TOKEN", None)
         NOSPAMPLUS_TOKEN = os.environ.get("NOSPAMPLUS_TOKEN", None)
         # Send .get_id in any group with all your administration bots (added)
@@ -174,7 +175,9 @@ elif ENV:
         DISABLE_MARKDOWN = os.environ.get("DISABLE_MARKDOWN", False)
         # Load Spammy Plugins, Which can be harmful.
         LOAD_OTHER_PLUGINS = os.environ.get("LOAD_OTHER_PLUGINS", False)
-        LOAD_OTHER_PLUGINS_CHNNL = os.environ.get("LOAD_OTHER_PLUGINS_CHNNL", "@WhiteEyePlugins")
+        LOAD_OTHER_PLUGINS_CHNNL = os.environ.get(
+            "LOAD_OTHER_PLUGINS_CHNNL", "@WhiteEyePlugins"
+        )
         #
         # DO NOT EDIT BELOW THIS LINE IF YOU DO NOT KNOW WHAT YOU ARE DOING
         # TG API limit. A message can have maximum 4096 characters!
