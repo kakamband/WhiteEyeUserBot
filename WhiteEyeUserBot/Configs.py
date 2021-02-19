bf0f2d8d185bc2da4033dbdc6517ec1d1ca9fdac
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-19 22:31:37 +0530

    Update Configs.py

--- WhiteEyeUserBot/Configs.py
+++ WhiteEyeUserBot/Configs.py
@@ -193,6 +193,7 @@ class Config(object):
     MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 3))
     # set to True if you want to log PMs to your PM_LOGGR_BOT_API_ID
     NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", False))
+    TG_BOT_USER_NAME_BF_HER = os.environ.get("TG_BOT_USER_NAME_BF_HER", None)
     # send .get_id in any channel to forward all your NEW PMs to this group
     PM_LOGGR_BOT_API_ID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
     if PM_LOGGR_BOT_API_ID:
