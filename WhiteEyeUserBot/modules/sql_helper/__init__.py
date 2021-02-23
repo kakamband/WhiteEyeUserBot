963a698c77a581106fbeb548b3b04cf8c2a7bea6
mrdayamzaidi <dayamali975@gmail.com>
2021-02-23 17:48:31 +0000

    pylint: WhiteEye🇮🇳

--- WhiteEyeUserBot/modules/sql_helper/__init__.py
+++ WhiteEyeUserBot/modules/sql_helper/__init__.py
@@ -1,8 +1,11 @@
 # -------------------------------------- #
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from WhiteEyeUserBot.db_start import BASE, SESSION
+
 from WhiteEyeUserBot.Configs import Config
+from WhiteEyeUserBot.db_start import BASE, SESSION
+
 # -------------------------------------- #
