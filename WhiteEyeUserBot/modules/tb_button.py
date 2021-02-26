5c731f7f841484aa90ccd294a08d2f799b7962af
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-02-26 16:10:04 +0530

    Update tb_button.py

--- WhiteEyeUserBot/modules/tb_button.py
+++ WhiteEyeUserBot/modules/tb_button.py
@@ -12,7 +12,7 @@ BTN_URL_REGEX = re.compile(r"(\{([^\[]+?)\}\<buttonurl:(?:/{0,2})(.+?)(:same)?\>
 
 @WhiteEye.on(WhiteEye_on_cmd(pattern="cbutton"))  # pylint:disable=E0602
 async def _(event):
-    if Config.TG_BOT_USER_NAME_BF_HER is None or tgbot is None:
+    if Config.TG_BOT_USERNAME is None or tgbot is None:
         await event.edit("need to set up a @BotFather bot for this module to work")
         return
 
